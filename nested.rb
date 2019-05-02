b0a88f525f3e0015d9d3ff1b9e983dcb7288a4bb
feven <feventsegay6@gmail.com>
2019-05-02 04:37:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,7 @@ def changing_alan
 		programmer_hash
 end
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -126,7 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-      programmer_hash[:alan_kay][:languages] << "Assembly"
-       
-     programmer_hash
+
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
+
