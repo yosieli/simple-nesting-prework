df7bc1e2ff815652d530a622cc2a5b9bd6b76225
feven <feventsegay6@gmail.com>
2019-05-02 04:32:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,9 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash.each do|name|
-      programmer_hash[:alan_kay][:languages].push(name)  
+      programmer_hash[:alan_kay][:languages]="Assembly" 
        
      programmer_hash
-   end
 end
